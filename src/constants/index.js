e291e5b1991b3e3c700aedee75059ad2fc878c8d
Aakashsingh Rajput <aakashrajput.amcec@gmail.com>
2025-05-16 05:14:23 +0530

    Merge remote and local

--- src/constants/index.js
+++ src/constants/index.js
@@ -49,19 +49,7 @@ const services = [
     title: "Full Stack Developer",
     icon: web,
   },
-  {
-    title: "Frontend Developer",
-    icon: mobile,
-  },
-  {
-    title: "Backend Developer",
-    icon: backend,
-  },
-  {
-    title: "Junior Software Developer",
-    icon: creator,
-  },
-];
+ ];
 
 const technologies = [
   {
@@ -140,26 +128,10 @@ const testimonials = [
   {
     testimonial:
       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    name: "Test1",
+    designation: "HR",
+    company: "Test Limited",
+    image: "Test.jpg",
   },
 ];
 
